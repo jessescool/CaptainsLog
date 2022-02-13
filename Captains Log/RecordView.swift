281f0e470705eef04dde7861e83562f08fa1b5c5
Jesse Cool <jessescool@gmail.com>
2022-02-13 15:12:54 -0800

    Naive speech working.... transcription only.

--- Captains Log/RecordView.swift
+++ Captains Log/RecordView.swift
@@ -1,33 +1,65 @@
 import SwiftUI
+import AVFoundation
 import RealmSwift
 
 struct RecordView: View {
     @Binding var showingRecordView: Bool
     @ObservedRealmObject var newLog = LogEntry()
+    @StateObject var speechRecognizer = SpeechRecognizer()
+    @State private var isRecording: Bool = false
     
     var body: some View {
         VStack {
             Text("New Log").font(.title).bold().padding(.top)
             Spacer()
+            
             VStack {
+                if isRecording {
+                    Text("I am recording!")
+                } else {
+                    Text("Not recording...")
+                }
+                Text(speechRecognizer.transcript).font(.headline).padding()
                 Text(newLog.date.formatted(date: .numeric, time: .shortened))
                 Text("\(newLog.location)")
                 TextField("Name", text: $newLog.name)
                 TextField("Transcription", text: $newLog.transcription)
             }
             
-            Button("Confirm") {
-                pushToStorage(log: newLog)
-                showingRecordView = false
-            }
-                .buttonStyle(.bordered)
+            Spacer()
             
-            Button("Cancel", role: .destructive) {
-                showingRecordView = false
+            VStack {
+                
+                HStack {
+                    Button("Transcribe") {
+                        speechRecognizer.reset()
+                        speechRecognizer.transcribe()
+                        isRecording = true
+                    }.buttonStyle(.bordered)
+                    Button("Stop transcribing", role: .destructive) {
+                        speechRecognizer.stopTranscribing()
+                        isRecording = false
+                    }
+                }
+                
+                HStack {
+                    Button("Confirm") {
+                        pushToStorage(log: newLog)
+                        showingRecordView = false
+                    }
+                        .buttonStyle(.bordered)
+                    
+                    Button("Cancel", role: .destructive) {
+                        showingRecordView = false
+                    }
+                }
+                
             }
+                
             Spacer()
         }
         .padding()
+
     }
 }
 
