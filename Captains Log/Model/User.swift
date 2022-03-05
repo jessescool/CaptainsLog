9e595ae89b5107f51a95ae65b0416d96b6ca68f8
Jesse Cool <jessescool@gmail.com>
2022-03-04 21:33:08 -1000

    Pre location marker

--- Captains Log/Model/User.swift
+++ Captains Log/Model/User.swift
@@ -7,20 +7,20 @@ class User: Object, Identifiable {
     @Persisted(primaryKey: true) var id: UUID
     @Persisted var username: String
     @Persisted var customWords = List<String>()
-    
+
     // Added later:
     @Persisted var tags = List<Tag>()
     var statistics = Statistics()
-    
+
     // naively appends a tag to tags
     func addTag(_ tag: Tag) {
         tags.append(tag)
     }
-    
+
     func addCustomWord(_ word: String) {
         customWords.append(word)
     }
-    
+
     convenience init(username: String) {
         self.init()
         self.id = UUID()
@@ -28,7 +28,6 @@ class User: Object, Identifiable {
     }
 
 }
-
 
 class Tag: EmbeddedObject {
     @Persisted var name: String
